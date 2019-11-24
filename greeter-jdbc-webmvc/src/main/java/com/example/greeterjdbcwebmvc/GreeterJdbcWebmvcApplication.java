8b80da71ae160d1278e6cf8f88def2eeb9cb6025
Adib Saikali <asaikali@gmail.com>
2019-11-23 22:06:57 -0500

    Add an application that connects to the h2 server

---
+++ greeter-jdbc-webmvc/src/main/java/com/example/greeterjdbcwebmvc/GreeterJdbcWebmvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.greeterjdbcwebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreeterJdbcWebmvcApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GreeterJdbcWebmvcApplication.class, args);
+  }
+
+}
--- h2-server/src/main/java/com/example/h2server/H2Server.java
+++ h2-server/src/main/java/com/example/h2server/H2Server.java
@@ -6,6 +6,6 @@ import org.h2.tools.Console;
 
 public class H2Server {
   public static void main(String[] args) throws SQLException {
-    Console.main("-webPort", "6565", "-tcpPort", "9096", "-pgPort","5436");
+    Console.main( "-webPort", "6565", "-tcpPort", "9096", "-pgPort","5436");
   }
 }
